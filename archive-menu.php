d3816f6d74391dd4825fbd91296a6f21c1e70582
Iitsuka Kazumi <ii.k.ne.1111@gmail.com>
2025-05-17 10:44:23 +0900

    ループを記述しました。

--- archive-menu.php
+++ archive-menu.php
@@ -23,99 +23,10 @@
                         <?php get_template_part('template-parts/loop', 'menu'); ?>
                     </section>
                     <?php endwhile; ?>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee02.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee03.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee04.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee05.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee06.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
-
-                    <section class="menu-item">
-                        <div class="menu-item-img">
-                            <img width="560" height="560" src="img/pic-menu-coffee07.jpg" alt="エントランスブレンドのコーヒー豆の画像"
-                                loading="lazy" />
-                        </div>
-
-                        <div class="menu-item-info">
-                            <h2 class="menu-item-name">エントランスブレンド</h2>
-                            <p class="menu-item-price">¥800</p>
-                            <p class="menu-item-description">
-                                親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。
-                            </p>
-                        </div>
-                    </section>
                 </div>
             </div>
             <?php endif; ?>
+
         </div>
     </div>
     <!-- end menu -->
