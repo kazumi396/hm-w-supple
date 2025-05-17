aa24e047183f08923b8a99fcc61cb54b7517df34
Iitsuka Kazumi <ii.k.ne.1111@gmail.com>
2025-05-17 14:45:25 +0900

    カスタムフィールドのコードを記述しました。

---
+++ template-parts/loop-shoplist.php
@@ -0,0 +1,16 @@
+<div class="shoplist-item-img">
+    <img src="img/pic-shoplist-kitasenju.jpg" alt="北千住店の外観の写真。〇〇な雰囲気の外観です。" width="648" height="410" loading="lazy" />
+</div>
+
+<div class="shoplist-item-info">
+    <h2 class="shoplist-item-name"><?php the_title(); ?></h2>
+
+    <p class="shoplist-item-adress"><?php the_field('address'); ?></p>
+    <p class="shoplist-item-tel"><?php the_field('tel'); ?></p>
+
+    <ul class="shoplist-item-detail">
+        <li class="shoplist-item-detail-item">営業時間／<?php the_field('time'); ?></li>
+        <li class="shoplist-item-detail-item">席数／<?php the_field('number'); ?>席</li>
+        <li class="shoplist-item-detail-item">喫煙／可</li>
+    </ul>
+</div>
