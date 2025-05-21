428a0edfc5cbe78eb028ea83c94377ba8c791694
Iitsuka Kazumi <ii.k.ne.1111@gmail.com>
2025-05-21 10:37:10 +0900

    種類別に表示をブロック分けしました。

--- archive-menu.php
+++ archive-menu.php
@@ -15,18 +15,43 @@
             <p class="menu-head-text">使用しているコーヒー豆を紹介します</p>
             <p class="menu-head-text02"><span>SUPPLEでは上質なコーヒー豆を</span><span>世界中から直接輸入しています。</span></p>
 
-            <?php if (have_posts()): ?>
-            <div class="u-mt">
-                <div class="menu-list">
-                    <?php while (have_posts()) : the_post(); ?>
-                    <section class="top-menu-block archive-menu-style menu-item">
-                        <?php get_template_part('template-parts/loop', 'menu'); ?>
-                    </section>
-                    <?php endwhile; ?>
+            <?php
+            $method_terms = get_terms(['taxonomy' => 'method' ]);
+            if ( !empty($method_terms)):
+            ?>
+            <div class="u-mt menu-block-wrap">
+                <?php foreach ($method_terms as $method): ?>
+                <div class="menu-block">
+                    <h2 class="menu-title">
+                        <span><?php echo strtoupper($method->slug); ?></span>
+                    </h2>
+                    <div class="menu-list">
+                        <?php
+                        $args = [
+                            'post_type' => 'menu',
+                            'posts_per_page' => -1,
+                        ];
+                        $taxquerysp = ['relation' => 'AND'];
+                        $taxquerysp[] = [
+                            'taxonomy' => 'method',
+                            'terms' => $method->slug,
+                            'field' => 'slug',
+                        ];
+                        $args['tax_query'] = $taxquerysp;
+                        $the_query = new WP_Query($args);
+                        if ($the_query->have_posts()): ?>
+                        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                        <section class="tax-menu-item">
+                            <?php get_template_part('template-parts/loop', 'menu'); ?>
+                        </section>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
+                <?php  endforeach; ?>
+                <?php endif; ?>
             </div>
-            <?php endif; ?>
-
         </div>
     </div>
     <!-- end menu -->
